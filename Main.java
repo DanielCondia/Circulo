483fdc429a7dc3e0d917c51c4c881d5c631b5b78
Daniel Condía Figueredo <daniel.condia@gmail.com>
2023-01-25 13:11:57 -0500

    Agregamos nuevo codigo mejor estructurado para circulo mas redondo, falta eliminar los espacios vacios

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
         char[][] array = new char[14][14];
         int contador = 0, controlador = 1, varTemporalSuma = (array.length / 2) + 4, varTemporalResta = (array.length / 2) - 4;
         for (int i = 0; i < array.length; i++) {
-            if(controlador == 3){
+            if(controlador == 2){
                 contador = i;
                 break;
             }
@@ -18,16 +18,38 @@ public class Main {
                         array[i][varTemporalResta] = '*';
                         array[i][varTemporalSuma] = '*';
                         varTemporalSuma++; varTemporalResta--;
-                        break;
                     }
                     else{
                         controlador++;
-                        if(controlador < 3){
-                            array[i][varTemporalResta] = '*';
-                            array[i][varTemporalSuma] = '*';
-                            break;
-                        }
+                        array[i][varTemporalResta] = '*';
+                        array[i][varTemporalSuma] = '*';
                     }
+                    break;
+                }
+            }
+        }
+        int segundoControlador = 0, segundaVarTempRest = (array.length / 2) - 4, segundaVarTempSum = (array.length / 2) + 4;
+        for (int i = array.length; i >= contador ; i--) {
+            if(segundoControlador == 2){
+                break;
+            }
+            for (int j = 0; j < array.length; j++) {
+                if(i == array.length - 1){
+                    if(j > (array.length / 2) - 3 && j < (array.length / 2) + 3){
+                        array[i][j] = '*';
+                    }
+                } else if (i < array.length - 1 && i > contador) {
+                    if (segundaVarTempRest > 0 && segundaVarTempSum < array.length - 1){
+                        array[i][segundaVarTempRest] = '*';
+                        array[i][segundaVarTempSum] = '*';
+                        segundaVarTempRest--; segundaVarTempSum++;
+                    }
+                    else{
+                        segundoControlador++;
+                        array[i][segundaVarTempRest] = '*';
+                        array[i][segundaVarTempSum] = '*';
+                    }
+                    break;
                 }
             }
         }
